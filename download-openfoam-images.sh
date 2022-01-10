a2a3060bde900890b3b853aa33b23e178bdbf4e7
jtorrex <jtorrex@posteo.net>
2022-01-10 11:16:30 +0000

    Moved download script

--- download-openfoam-images.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-singularity pull library://fertinaz-hpc/openfoam/of-7.sif:sha256.87a06205d8f66a4d3c2391e1a8eed8358e85de63588682e398fa81eded65d417
-mv of-7.sif_sha256.87a06205d8f66a4d3c2391e1a8eed8358e85de63588682e398fa81eded65d417.sif singularity-images/of-7.sif
